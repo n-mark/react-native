ba713e41e84b5426ee0c1a24a1c267729ca449f4
{n-mark} <{markovskinikita@gmail.com}>
2026-09-13 14:50:13 +0300

    user form

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,62 @@
 import { useState } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+interface Form {
+  name: string;
+  surname: string;
+  email: string;
+}
 
 export default function HomeScreen() {
-  /**
-   * React Hooks like the useState and useEffect functions identically in React Native.
-   */
-  const [username, setUsername] = useState('');
+  const [form, setForm] = useState<Form>({
+    name: '',
+    surname: '',
+    email: ''
+  });
+
+  const [displayForm, setDisplayForm] = useState<Form>({
+    name: '',
+    surname: '',
+    email: ''
+  });
+
+  const handleChange = (field: keyof Form, value: string | number) => {
+    setForm(prev => ({ ...prev, [field]: value }));
+  };
+
+  const copyToDisplay = (newForm: Form) => {
+    setDisplayForm(newForm)
+  }
 
   return (
     <View>
-      <TextInput value={username} onChangeText={(value) => setUsername(value)}/>
-
-{/* Component composition, props passing,
-conditional rendering, and lists also work the same way. */}
-      <Text>{username === '' ? 'Please enter your name' : `Welcome, ${username}!`}</Text>
+      <View style={{ padding: 60 }}>
+        <Text style={styles.title}>INFO</Text>
+        <Text>Name: {displayForm.name}</Text>
+        <Text>Surname: {displayForm.surname}</Text>
+        <Text>Email: {displayForm.email}</Text>
+      </View>
+      <View style={{ padding: 60 }}>
+        <Text style={styles.title}>Please tell us about you</Text>
+        <TextInput placeholder='Enter your name' onChangeText={(value) => handleChange('name', value)} />
+        <TextInput placeholder='Enter your surname' onChangeText={(value) => handleChange('surname', value)} />
+        <TextInput placeholder='Enter your email' onChangeText={(value) => handleChange('email', value)} />
+        <Button title="Submit" onPress={() => copyToDisplay(form)}/>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+  },
+  body: {
+    fontSize: 16,
+    color: '#666',
+    lineHeight: 22,
+  },
+});
