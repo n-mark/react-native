e56025c9dab0eb8def68c1ef104df27aecbfe951
{n-mark} <{markovskinikita@gmail.com}>
2026-09-13 12:40:10 +0300

    React Concepts in React Native

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,38 +1,19 @@
-import { View, Text, Image, TextInput, ScrollView, Button, Alert } from 'react-native';
-
-const food = [
-  'Pizza', 'Burger', 'Pasta', 'Salad', 'Sushi',
-  'Tacos', 'Sandwich', 'Ramen', 'Chicken',
-  'Fish', 'Rice', 'Noodles', 'Soup', 'Bread',
-  'Cheese', 'Eggs', 'Pancakes', 'Waffles'
-];
+import { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 export default function HomeScreen() {
+  /**
+   * React Hooks like the useState and useEffect functions identically in React Native.
+   */
+  const [username, setUsername] = useState('');
+
   return (
     <View>
+      <TextInput value={username} onChangeText={(value) => setUsername(value)}/>
 
-  {/* The <TextInput> component allows users to enter text,
-  similar to an HTML input or textarea.
-  It captures user input and can update the state as the user types */}
-      <TextInput placeholder='Enter your name' />
-
-  {/* The <ScrollView> component provides a scrollable container when
-  content exceeds the screen size, think of a social media feed!
-  Unlike <View>, which shows a fixed area, <ScrollView> allows users to scroll
-  through long lists, images, or text content */}
-      <ScrollView style={{ height: 100 }}>
-        {food.map((item, index) => (
-          <Text key={index}>{item}</Text>
-        ))}
-      </ScrollView>
-
-{/* The <Button> component provides a standard button that users can tap.
-  When pressed, we can link it to functions that perform actions like sending data,
-  navigating screens, or showing alerts: */}
-    <Button
-      title="Press me"
-      onPress={() => Alert.alert('Button pressed!')}
-    />
+{/* Component composition, props passing,
+conditional rendering, and lists also work the same way. */}
+      <Text>{username === '' ? 'Please enter your name' : `Welcome, ${username}!`}</Text>
     </View>
   );
 }
