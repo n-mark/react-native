059403be7a6bafe82116cf50d7d25d2ea869165b
{n-mark} <{markovskinikita@gmail.com}>
2026-09-14 19:28:35 +0300

    style merging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,38 +3,37 @@ import { View, StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.layout}>
-      <View style={styles.card} />
-      <View style={styles.card} />
-    </View>
+  <View style={styles.layout}>
+    <Box />
+  </View>
   );
 }
 
-/**
- * With the StyleSheet API, we can write our styling rules
- * separately and reference them when rendering the components.
- * These function similarly to CSS Stylesheets and allow styling
- * rules to be reused for multiple components.
- *
- * With the StyleSheet API, we can pull the inline styling
- * from our <AwesomeBox> component. Doing that allows us to
- * reuse the styling rules and keep our <AwesomeBox> component readable.
- */
-const styles = StyleSheet.create({
+export const Box = () => (
+  /**
+   * In React Native, we can apply multiple styles to a single component
+   * by passing an array of style objects, StyleSheet references,
+   * or a combination of both to the style prop. This approach is similar
+   * to applying multiple CSS classes to an HTML element,
+   * where each class contributes its own set of styles.
+   *
+   * When an array of styles is passed, React Native applies them from left to right,
+   * with the rightmost styles taking precedence and overriding
+   * any conflicting properties from earlier styles in the array.
+   */
+  <View style={[styles.box, { backgroundColor: 'blue' }]} />
+);
+
+
+export const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    backgroundColor: '#e5e5e5',
-    justifyContent: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  card: {
+  box: {
     width: 100,
     height: 100,
-    backgroundColor: 'white',
-    margin: 16,
-    borderRadius: 2,
-    shadowColor: 'black',
-    shadowOpacity: 0.3,
-    shadowRadius: 1,
-    shadowOffset: { height: 1, width: 0.3 }
-  }
+    backgroundColor: 'red',
+  },
 });
