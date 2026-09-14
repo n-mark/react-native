8d6474dd71f01f97335ef0a30d2fa499920d348a
{n-mark} <{markovskinikita@gmail.com}>
2026-09-14 20:54:52 +0300

    flex

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,53 +1,33 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
 
 
 export default function HomeScreen() {
   return (
-    <View style={styles.layout}>
-      <BoxOne isActive='true' />
-      <BoxTwo />
-    </View>
+  <View style={styles.layout}>
+    <View style={[styles.box, { backgroundColor: 'red' }]} />
+    <View style={[styles.box, { backgroundColor: 'green' }]} />
+    <View style={[styles.box, { backgroundColor: 'blue' }]} />
+  </View>
   );
 }
 
-export const BoxOne = (props) => (
-  /**
-   * In this example, when the nightMode prop is true,
-   * the background color is overridden to black;
-   * otherwise, it remains blue. This approach is particularly useful
-   * when we want to combine multiple base styles and selectively override
-   * specific properties based on a condition.
-   *
-   * Another common technique for dynamic styling is using
-   * the ternary operator to choose between two distinct style objects.
-   * This is especially useful when the styling needs to be mutually exclusive,
-   * such as toggling between a light and dark theme
-   */
-  <View style={[styles.redBox, props.isActive && styles.blueBox]} />
-);
 
-export const BoxTwo = (props) => (
-  <View style={props.isActive ? styles.blueBox : styles.redBox} />
-);
+// Get the maximum width/height (in dp) from the Dimensions API
+const MAX_WIDTH = Dimensions.get('window').width;
+const MAX_HEIGHT = Dimensions.get('window').height;
 
 export const styles = StyleSheet.create({
   layout: {
+    /**
+     * To make layouts dynamic, we use the flex property.
+     * This property allows child components to expand
+     * or shrink to fill available space along the main axis.
+     */
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#e5e5e5',
   },
-
-  redBox: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
-    borderWidth: 2,
-    borderColor: "white",
-  },
-
-  blueBox: {
-    width: 150,
-    height: 150,
-    backgroundColor: "blue",
+  box: {
+    flex: 1,
+    backgroundColor: 'black',
   },
 });
